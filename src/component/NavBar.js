c6ab1d10bd9ebf63f98f1771bb2d2b7bda9faad0
MANOJ KUMAR <manojkumarjana@MANOJ-Macair.local>
2024-03-14 16:23:41 +0530

    navbar update

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -8,7 +8,7 @@ export default class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark ">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">QuizQuest</Link>
+    <Link className="navbar-brand" to="/">QuizMania</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
